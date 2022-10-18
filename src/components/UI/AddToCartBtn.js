348187a7a056d291fca9f05dfa58553559aca80b
DanielS4323 <daky4323@gmail.com>
2022-10-18 14:55:46 +0200

    add to cart button

---
+++ src/components/UI/AddToCartBtn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './AddToCartBtn.module.css'
+
+const AddToCartBtn = () => {
+  return (
+    <button className={styles.btn}>Add To Cart</button>
+  )
+}
+
+export default AddToCartBtn
