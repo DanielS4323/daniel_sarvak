1df4cec9dac1bec9ed90114fd5177bc14abb9925
DanielS4323 <daky4323@gmail.com>
2022-10-18 14:54:06 +0200

    di passed

--- src/components/Products/ShowProducts.js
+++ src/components/Products/ShowProducts.js
@@ -3,8 +3,9 @@ import Product from "./Product";
 
 const ShowProducts = (props) => {
   const product = props.product;
+  const id = props.id
 
-  return <Product product={product} />;
+  return <Product id={id} product={product} />;
 };
 
 export default ShowProducts;
