d52f0ebaa2900dce63645d108c7d047db1aa68f0
DanielS4323 <daky4323@gmail.com>
2022-10-20 16:41:06 +0200

    cart modal

---
+++ src/components/UI/CartPop.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import cartSvg from "../../assets/icons/cart.svg";
+
+const style = {
+  position: "absolute",
+  top: "30%",
+  left: "75%",
+  transform: "translate(-30%, -75%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const btnStyle = {};
+
+export default function CartPop() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button sx={btnStyle} onClick={handleOpen}>
+        <img src={cartSvg} alt="cart" />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
