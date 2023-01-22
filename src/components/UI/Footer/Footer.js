92637a851174749f137aed82c234fface2aa8782
DanielS4323 <daky4323@gmail.com>
2023-01-22 19:34:07 +0100

    footer moved

---
+++ src/components/UI/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles}><p>2022 ®</p></footer>
+  )
+}
+
+export default Footer
