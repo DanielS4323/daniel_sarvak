9cfc32140245e0ba0b3b29d628a398d0149124c4
DanielS4323 <daky4323@gmail.com>
2023-01-22 19:34:59 +0100

    add funcs

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/CartSlice";
 import formatCurrency from "../../utilities/formatCurrency";
+import ProductImages from "../Product/ProductImages";
 import styles from "./CartItem.module.css";
+import carouselCartStyle from "./CarouselCart.module.css";
+const CartItem = ({ brand, name, price, image, quantity, id }) => {
+  const dispatch = useDispatch();
 
-const CartItem = ({ brand, name, price, image }) => {
+  const removeFromCartHanlder = () => {
+    dispatch(cartActions.removeFromCart(id));
+  };
+
   return (
     <div className={styles.wrap}>
       <li className={styles.listItem}>
         <p>{brand}</p>
         <p>{name}</p>
-        <p>{formatCurrency(price)}</p>
+        <p>
+          {formatCurrency(price)}
+          <span> x {quantity}</span>
+        </p>
       </li>
-      <img className={styles.image} src={image} alt="product" />
+      <ProductImages showThumbs={false} style={carouselCartStyle.carousel} images={image} />
+      {/* <img className={styles.image} src={image} alt="product" /> */}
+      <button className={styles.removeBtn} onClick={removeFromCartHanlder}>
+        Remove
+      </button>
     </div>
   );
 };
