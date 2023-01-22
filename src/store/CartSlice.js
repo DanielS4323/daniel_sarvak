9da5c448af64803bfa1526a86e417fcd5b23fe58
DanielS4323 <daky4323@gmail.com>
2023-01-22 19:37:42 +0100

    logic add/remove

--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -11,13 +11,39 @@ const cartSlice = createSlice({
   reducers: {
     addToCart(state, action) {
       const newItem = action.payload;
-      const existingItem  = state.myCart.find((item => item.id === newItem.id));
+      const existingItem = state.myCart.find((item) => item.id === newItem.id);
       state.totalItems++;
       if (!existingItem) {
         state.myCart.push({ ...newItem, quantity: 1 });
+        state.totalPrice = state.totalPrice + newItem.price;
       } else {
-        existingItem.quantity++
+        existingItem.quantity++;
+        state.totalPrice += existingItem.price;
       }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.myCart.find((item) => item.id === id);
+      if (!existingItem) {
+        return;
+      }
+      if (existingItem.quantity > 1) {
+        existingItem.quantity--;
+        state.totalPrice -= existingItem.price;
+        state.totalItems--;
+      } else {
+        state.myCart = state.myCart.filter((item) => item.id !== id);
+        state.totalPrice -= existingItem.price;
+        state.totalItems--;
+      }
+
+      // if (existingItem?.quantity === 1) {
+      //   state.myCart = state.myCart.filter((item) => item.id !== id);
+      //   state.totalPrice -= existingItem.price;
+      // } else {
+      //   existingItem.quantity--;
+      //   state.totalPrice -= existingItem.price;
+      // }
     },
   },
 });
