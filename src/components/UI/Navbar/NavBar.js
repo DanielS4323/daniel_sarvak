823fc3c249035cb8205301550f3eae5f11d8f8eb
DanielS4323 <daky4323@gmail.com>
2023-01-22 19:34:20 +0100

    navbar move

---
+++ src/components/UI/Navbar/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.css";
+import { useQuery } from "@apollo/client";
+import { GET_CATEGORIES } from "../../../GraphQL/Queries";
+import CartPop from "../CartPop/CartPop";
+import { useSelector } from "react-redux";
+
+const NavBar = () => {
+  const { error, loading, data } = useQuery(GET_CATEGORIES);
+
+  const totalItems = useSelector((state) => state.cart.totalItems);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      borderBottom: isActive ? "2px solid #5ECE7B" : "",
+      paddingBottom: isActive ? "10px" : "",
+      color: isActive ? "#5ECE7B" : "",
+    };
+  };
+
+  return (
+    <nav className={styles}>
+      <ul>
+        {data.categories.map((el) => (
+          <li key={el.name}>
+            <NavLink style={navLinkStyles} to={`/product/${el.name}`}>
+              {el.name}
+            </NavLink>
+          </li>
+        ))}
+        <div className={styles.cart}>
+          <li>currency</li>
+          <>
+            <CartPop />
+            {totalItems !== 0 && <span>{totalItems}</span>}
+          </>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
