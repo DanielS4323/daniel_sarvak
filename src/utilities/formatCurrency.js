75b0ef593ffd27daf4c6fd66d27f32f5ccf9108b
DanielS4323 <daky4323@gmail.com>
2023-01-28 21:13:47 +0100

    format currency fn

--- src/utilities/formatCurrency.js
+++ src/utilities/formatCurrency.js
@@ -1,8 +1,10 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat("en-IN", {
-  currency: "USD",
-  style: "currency",
-});
 
-export default function formatCurrency(number) {
+
+export default function formatCurrency(number, currency) {
+  const CURRENCY_FORMATTER = new Intl.NumberFormat("en-IN", {
+    currency: currency,
+    style: "currency",
+  });
+
   return CURRENCY_FORMATTER.format(number);
 }
