f6dc0f6a988c33da20292b1041d53516e98cc548
DanielS4323 <daky4323@gmail.com>
2023-01-28 17:55:55 +0100

    destruct state.cart

--- src/components/cart/CartItems.js
+++ src/components/cart/CartItems.js
@@ -5,19 +5,17 @@ import CartItem from "./CartItem";
 import styles from "./CartItems.module.css";
 
 const CartItems = () => {
-  const myCart = useSelector((state) => state.cart.myCart);
-  const totalPrice = useSelector((state) => state.cart.totalPrice);
+  const { myCart, totalPrice } = useSelector((state) => state.cart);
+
   const items = myCart.map((item) => <CartItem key={item.id} {...item} />);
 
   return (
-    <>
+    <React.Fragment>
       <ul className={styles.list}>{items}</ul>
       {totalPrice > 0 && (
-        <span className={styles.totalPrice}>
-          Total: {formatCurrency(totalPrice)}
-        </span>
+        <p className={styles.totalPrice}>Total: {formatCurrency(totalPrice)}</p>
       )}
-    </>
+    </React.Fragment>
   );
 };
 
