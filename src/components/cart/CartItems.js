8ebc0f63691ac3f2f29bd7639b0ce9d3148d08d7
DanielS4323 <daky4323@gmail.com>
2023-01-28 21:11:13 +0100

    total price amount implement

--- src/components/cart/CartItems.js
+++ src/components/cart/CartItems.js
@@ -3,17 +3,25 @@ import { useSelector } from "react-redux";
 import formatCurrency from "../../utilities/formatCurrency";
 import CartItem from "./CartItem";
 import styles from "./CartItems.module.css";
+import getTotalAmountCart from "../../utilities/getTotalAmountCart";
 
 const CartItems = () => {
-  const { myCart, totalPrice } = useSelector((state) => state.cart);
+  const { myCart, currency } = useSelector((state) => state.cart);
 
   const items = myCart.map((item) => <CartItem key={item.id} {...item} />);
+
+  // console.log(items[0]);
+
+  // console.log(12,findCurrency(items.prices, currency))
 
   return (
     <React.Fragment>
       <ul className={styles.list}>{items}</ul>
-      {totalPrice > 0 && (
-        <p className={styles.totalPrice}>Total: {formatCurrency(totalPrice)}</p>
+      {myCart.length > 0 && (
+        <p className={styles.totalPrice}>
+          Total:{" "}
+          {formatCurrency(getTotalAmountCart(myCart, currency), currency)}
+        </p>
       )}
     </React.Fragment>
   );
