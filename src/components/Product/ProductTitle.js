9f0149ee847e04e19256fe5712ff01ceda60181b
DanielS4323 <daky4323@gmail.com>
2023-01-28 21:11:49 +0100

    format and find currency

--- src/components/Product/ProductTitle.js
+++ src/components/Product/ProductTitle.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import formatCurrency from "../../utilities/formatCurrency";
+import findCurrency from "../../utilities/findCurrency";
 import styles from "./ProductTitle.module.css";
 
-const ProductTitle = ({ brand, name, price }) => {
+const ProductTitle = ({ brand, name, prices }) => {
+  const selectedCurrency = useSelector((state) => state.cart.currency);
+
   return (
     <div className={styles.wrap}>
       <p className={styles.productBrand}>{brand}</p>
       <p className={styles.productName}>{name}</p>
-      <p className={styles.productPrice}>{formatCurrency(price)}</p>
+      <p className={styles.productPrice}>
+        {formatCurrency(
+          findCurrency(prices, selectedCurrency),
+          selectedCurrency
+        )}
+      </p>
     </div>
   );
 };
